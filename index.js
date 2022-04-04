c94d519e3e2aa03bf4bc0cfb48591c12f460f651
Akku <36289786+genesis2907@users.noreply.github.com>
2022-04-05 01:44:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var app = express()
+
+var users = require('./routes/users')
+app.use('/users', users)
+
+var products = require('./routes/products')
+app.use('/products', products)
+
+var orders = require('./routes/orders')
+app.use('/orders', orders)
+
+app.get
+ ('/', (req,res) => 
+    {
+        res.send('This is "index.js" endpoint')
+    }
+)
+
+app.get
+ ('/*', (req,res) => 
+    {
+        res.send('404: URL not found.')
+    }
+)
+
+setTimeout
+(
+    function() 
+    {
+        console.log('Please wait...')
+    }, 
+    1000
+)
+app.listen(7000)
+setTimeout
+(
+    function() 
+    {
+        console.log('Listening on port 7000...')
+    }, 
+    5000
+)
